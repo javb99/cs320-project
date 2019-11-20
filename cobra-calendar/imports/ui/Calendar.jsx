9f2eb617979f89ed7d05ab4fa980e3b70266ecf7
Joe Van Boxtel <joe@pndlm.com>
2019-11-19 18:39:00 -0800

    Add the 15minute increments.

--- cobra-calendar/imports/ui/Calendar.jsx
+++ cobra-calendar/imports/ui/Calendar.jsx
@@ -16,17 +16,36 @@ const Calendar = () => {
   )
 }
 
-
 const Day = (props) => {
+    const segments = ["00", "15", "30", "45"];
     const times = ["8", "9", "10", "11", "12", "1", "2", "3", "4", "5", "6", "7", "8"]
     return (
         <Grid columns={1}>
           <h1>{props.name}</h1>
-          {times.map( (time, index) => (
-              <Grid.Column key={index}>{time}</Grid.Column>
+          {times.map( (time, row) => (
+              <Grid.Column key={row}>
+                <Grid>
+                  {segments.map( (label, column) => {
+                    return <Grid.Column>
+                      {militaryTimeForIndex(row*segments.length + column)}
+                    </Grid.Column>;
+                  })}
+                </Grid>
+              </Grid.Column>
           ))}
         </Grid>
     );
 }
 
 export default Calendar;
+
+// Takes an index in the 4/13 array representing 8am to 8pm and returns the integer representing the 24 hour time.
+// For example index 0 -> 800, index 51 -> 2045
+function militaryTimeForIndex(index) {
+  const minHour = 8;
+  const columnCount = 4;
+  const segmentLength = 15;//60 / columnCount;
+  const row = Math.floor(index / columnCount);
+  const col = index % columnCount;
+  return col * segmentLength + (100 * (row + minHour));
+}
