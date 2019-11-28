d46b99dab64c14a723f824aef525d6573e1f1195
Joe Van Boxtel <joe@pndlm.com>
2019-11-28 11:11:06 -0800

    Add the beginning of a custom log in screen.

--- cobra-calendar/imports/ui/App.jsx
+++ cobra-calendar/imports/ui/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import CalendarScreen from './CalendarScreen';
 import LogInScreen from './LogInScreen';
 
 const App = () => (
   <div>
+    <LogInScreen/>
     <CalendarScreen/>
   </div>
 );
