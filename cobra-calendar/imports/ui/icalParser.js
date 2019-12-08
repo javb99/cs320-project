2928845a2f97d236ef4833e05da1157aa1d9f8e1
Jakobminer <jakobminer25@gmail.com>
2019-12-08 15:40:51 -0800

    accounted for async importer

--- cobra-calendar/imports/ui/icalParser.js
+++ cobra-calendar/imports/ui/icalParser.js
@@ -1,25 +1,29 @@
 import ical from 'ical';
 
-function parseCalendar (url) {
+async function parseCalendar (url) {
   const eventList = [];
-  ical.fromURL(url, {}, function (err, data) {
-    for (let k in data) {
-      if (data.hasOwnProperty(k)) {
-        const ev = data[k];
-        if (data[k].type === 'VEVENT') {
-          const userEvent = {
-            eventStart: ev.start,
-            eventEnd: ev.end
-          };
-          eventList.push(userEvent);
-        }
-      }
-    }
-    console.log(eventList.length + " inside fromURL");
-  });
-  console.log(eventList.length + " inside parse");
+  await icalParse(url, eventList);
   return eventList;
 }
-
-let events = parseCalendar('https://learn.wsu.edu/webapps/calendar/calendarFeed/4ed515a26d2b4187bb65e957a6bb8a43/learn.ics');
-console.log(events.length + " after return");
+function icalParse(url, eventList) {
+  return new Promise( resolve => {
+    ical.fromURL(url, {}, function (err, data) {
+      for (let k in data) {
+        if (data.hasOwnProperty(k)) {
+          const ev = data[k];
+          if (data[k].type === 'VEVENT') {
+            const userEvent = {
+              eventStart: ev.start,
+              eventEnd: ev.end
+            };
+            eventList.push(userEvent);
+          }
+        }
+      }
+      console.log(eventList.length + " inside fromURL");
+      resolve();
+    });
+  });
+}
+let eventsPromise = parseCalendar('https://learn.wsu.edu/webapps/calendar/calendarFeed/4ed515a26d2b4187bb65e957a6bb8a43/learn.ics');
+eventsPromise.then((events) => {console.log(events.length + " after return");});
