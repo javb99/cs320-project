3f3100b67d1cde9f76901247fc641263bd2bab12
Joseph Van Boxtel <dev.josephvanboxtel@gmail.com>
2019-12-12 13:56:05 -0800

    Remove magic constants for 60 and 100 in TimeSlotFactory

--- cobra-calendar/imports/presentation/ConcreteTimeSlotFactory.js
+++ cobra-calendar/imports/presentation/ConcreteTimeSlotFactory.js
@@ -1,10 +1,13 @@
 import * as _ from 'underscore';
 
+const base10Hour = 100;
+const base60Hour = 60;
+
 export default class ConcreteTimeSlotFactory {
   constructor(slotsPerHour, emptyColor, emptyDescription, firstHour, hourCount) {
-    console.assert(60 % slotsPerHour === 0, 'slotsPerHour must divide 60 minutes evenly');
+    console.assert(base60Hour % slotsPerHour === 0, 'slotsPerHour must divide 60 minutes evenly');
     this.slotsPerHour = slotsPerHour;
-    this.slotLength = 60 / slotsPerHour
+    this.slotLength = base60Hour / slotsPerHour
     this.emptySlot = { color: emptyColor, description: emptyDescription };
     this.firstHour = firstHour;
     this.hourCount = hourCount;
@@ -19,21 +22,20 @@ export default class ConcreteTimeSlotFactory {
     return timeSlots
   }
   militaryTimeFor(row, column) {
-    return column * this.slotLength + (100 * (row + this.firstHour));
+    return column * this.slotLength + (base10Hour * (row + this.firstHour));
   }
 }
 
-
 export const normalizeEvents = (events, slotsPerHour) => {
   return events.map((event)=>{
-    const slotLength = 60 / slotsPerHour;
+    const slotLength = base60Hour / slotsPerHour;
     let start = militaryTimeForDate(event.start);
-    const startMin = start % 100;
+    const startMin = start % base10Hour;
     if (startMin % slotLength > 0) {
       start = start - startMin % slotLength; // Round down to nearest slot
     }
     let end = militaryTimeForDate(event.end);
-    const endMin = end % 100;
+    const endMin = end % base10Hour;
     if (endMin % slotLength > 0) {
       end = end + (slotLength - endMin % slotLength); // Round up to nearest slot.
     }
@@ -41,15 +43,13 @@ export const normalizeEvents = (events, slotsPerHour) => {
   })
 };
 
-const hour = 100;
-
 const slotStartsFor = (start, end, slotLength) => {
   let times = [];
   for (let time = start; time < end;) {
     times.push(time);
     time += slotLength;
-    if (time % hour >= 60) { // Check for 61 to 100, should increment the hour.
-      time = (Math.floor(time / hour) + 1) * hour;
+    if (time % base10Hour >= base60Hour) { // Check for 61 to 100, should increment the hour.
+      time = (Math.floor(time / base10Hour) + 1) * base10Hour;
     }
   }
   return times;
@@ -57,7 +57,7 @@ const slotStartsFor = (start, end, slotLength) => {
 
 const splitEventsToSlots = (normalizedEvents, slotsPerHour) => {
   const eventSlots = _.flatten(_.map(normalizedEvents, (event)=>{
-    let slotLength = 60 / slotsPerHour;
+    let slotLength = base60Hour / slotsPerHour;
     const starts = slotStartsFor(event.start, event.end, slotLength);
     return _.map(starts, (aStart)=>{
       return { color: event.color, start: aStart, description: event.description };
@@ -71,5 +71,5 @@ export const slotsForRawEvents = (events, slotsPerHour) => {
 };
 
 function militaryTimeForDate(date) {
-  return date.getHours() * 100 + date.getMinutes()
+  return date.getHours() * base10Hour + date.getMinutes()
 }
